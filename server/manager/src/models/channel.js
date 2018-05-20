3800cfecab3ceee94fd435a9c62994782e6fa20b
zhangxing <zhangxing@licai.com>
2018-05-20 16:41:39 +0800

    add and delete channel functions

--- server/manager/src/models/channel.js
+++ server/manager/src/models/channel.js
@@ -1,36 +1,39 @@
-import {getChannelList,deleteChannel} from '../services/channel'
+import {getChannelList,addChannel,updateChannel,deleteChannel} from '../services/channel'
 
 export default {
   namespace: 'channel',
 
-  state: {
-    data:{ 
+  state: {  
+    data:{  
       list: [],
       pagination: {  
-      },
-    } 
+      }, 
+    }
   },
   effects: {
-    *fetch({ payload }, { call, put }) {
-      console.log('------query--list--data--',payload)
-      const response = yield call(getChannelList, payload);   
+    *fetch({ payload }, { call, put }) { 
+      const response = yield call(getChannelList, payload);    
       yield put({
         type: 'save',
-        payload: response.data
-      });
+        payload:{list:response.data,pagination:{total:100,current:1}}
+      }); 
     },
-    *delete({ payload, callback }, { call, put }) {
-      console.log('--freeze---payload----',payload)
-      const response = yield call(deleteChannel, payload);   
+    *add({payload,callback},{call,put}){ 
+      const response=yield call(addChannel,payload);
+      console.log('add function response-',response)
+      if (callback) callback();
+    },
+    *delete({ payload, callback }, { call, put }) {  
+      const response = yield call(deleteChannel, payload.id);    
       if (callback) callback();
     },
   },
 
   reducers: {
-    save(state, action) {
+    save(state, action) { 
       return {
         ...state,
-        data: action.payload,
+        data:action.payload,
       };
     }
   },
