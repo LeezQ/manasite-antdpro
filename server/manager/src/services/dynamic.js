78ce6bf37fa24f53baacbd6763fb863e3bc5a7d0
fively0124 <fively0124@126.com>
2018-07-15 21:32:53 +0800

    动态管理(置换管理，活动管理，分享管理)绑定后端接口

--- server/manager/src/services/dynamic.js
+++ server/manager/src/services/dynamic.js
@@ -6,8 +6,22 @@ import request from '../utils/request';
  * @return {[type]}       [description]
  */
 export async function getExchangeList(param){
-    return request('/api/channels',{
-      method:'POST',
-      body:param
+    return request('/api/discover/channel',{
+      method:'GET',
+      ...param
+    })
+}
+
+export async function getActivityList(param){
+    return request('/api/discover/channel',{
+      method:'GET',
+      ...param
+    })
+}
+
+export async function getShareList(param){
+    return request('/api/discover/channel',{
+      method:'GET',
+      ...param
     })
 }
