d3c35555be3398992791b07dcc11c3fe11248386
allen <bolansiji@163.com>
2018-07-19 10:14:35 +0800

    feat: add search component

---
+++ server/manager/src/components/SearchForm/index.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { Row, Col, Form, Input, Button } from 'antd';
+import AddUserForm from 'components/System/AddForm';
+
+import styles from './index.less';
+
+const FormItem = Form.Item;
+
+@Form.create()
+export default class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
+  handleModalVisible = () => {
+    this.setState({ visible: true });
+  };
+
+  hidenModal = () => {
+    this.setState({ visible: false });
+  };
+
+  handleSave = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+      console.log('Received values of form: ', values);
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+  };
+
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
+
+  handleFormReset = () => {
+    const { form } = this.props;
+    form.resetFields();
+  };
+
+  renderForm() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Col md={8} sm={24}>
+            <FormItem label="用户名">
+              {getFieldDecorator('user_name')(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="姓名">
+              {getFieldDecorator('name')(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="角色">
+              {getFieldDecorator('role')(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+        </Row>
+        <div style={{ overflow: 'hidden' }}>
+          <span style={{ float: 'right', marginBottom: 24 }}>
+            <Button type="primary" htmlType="submit">
+              查询
+            </Button>
+            <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+              重置
+            </Button>
+            <Button style={{ marginLeft: 8 }} type="primary" onClick={this.handleModalVisible}>
+              新增用户
+            </Button>
+          </span>
+        </div>
+      </Form>
+    );
+  }
+  render() {
+    const { visible } = this.state;
+    return (
+      <div className={styles.tableListForm}>
+        {this.renderForm()}
+        <AddUserForm
+          wrappedComponentRef={this.saveFormRef}
+          visible={visible}
+          onCancel={this.hidenModal}
+          onCreate={this.handleSave}
+        />
+      </div>
+    );
+  }
+}
--- server/manager/src/components/System/AddRoleForm.js
+++ server/manager/src/components/System/AddRoleForm.js
@@ -2,64 +2,50 @@
    由zhaojunzhe于2018/7/14创建
 */
 import React, { Component } from 'react';
-import {
-  Checkbox,
-  Form,
-  Input,
-  Modal,
-  Select,
-  Button,
-  Icon,
-  Dropdown,
-  Menu
-} from 'antd';
-import {getChannelList} from '../../services/channel';
-import {getMenu, roles} from '../../services/system'
-import ProvinceSelect from './ProvinceSelect.js'
+import { Checkbox, Form, Input, Modal, Button, Icon, Dropdown, Menu, Row, Col } from 'antd';
+import { getChannelList } from '../../services/channel';
+import { getMenu, roles } from '../../services/system';
+import ProvinceSelect from './ProvinceSelect.js';
 
 const FormItem = Form.Item;
-const Option = Select.Option;
 const CheckboxGroup = Checkbox.Group;
 
-
-
-class AddRoleForm extends Component{
-  
-  state={
+class AddRoleForm extends Component {
+  state = {
     channels: [],
-    menu: []
-  }
-  componentDidMount(){
+    menu: [],
+  };
+  componentDidMount() {
     this.getChannel();
     this.getMenu();
   }
   getMenu = () => {
-    getMenu().then((data) => {
+    getMenu().then(data => {
       if (data.status === 'ok') {
-        this.setState({menu: data.data.list});
+        this.setState({ menu: data.data.list });
       }
     });
   };
   getChannel = () => {
-    getChannelList().then((data) => {
+    getChannelList().then(data => {
       if (data.status === 'ok') {
-        const channels = data.data.map((item) => {
+        const channels = data.data.map(item => {
           const obj = {};
           obj.label = item.name;
           obj.value = item.id;
           return obj;
         });
-        this.setState({channels: channels});
+        this.setState({ channels });
       }
-    })
+    });
   };
-  handleChanelChange = (checkedValues) => {
-    console.log(checkedValues)
+  handleChanelChange = checkedValues => {
+    console.log(checkedValues);
   };
-  handleParentChange = (e) => {
-    console.log(e)
+  handleParentChange = e => {
+    console.log(e);
   };
-  handleChange = (value) => {
+  handleChange = value => {
     const obj = {};
     obj.province = value[0];
     obj.city = value[1];
@@ -72,80 +58,86 @@ class AddRoleForm extends Component{
         obj.name = values.name;
         obj.location = {
           province: '河北省',
-          city: '石家庄市'
+          city: '石家庄市',
         };
         obj.channels = values.channels;
-        roles(obj).then((data) => {
+        roles(obj).then(data => {
           console.log(data);
-        })
+          this.props.onCreate();
+        });
         console.log('Received values of form: ', values);
       }
     });
   };
-  render(){
-    const {channels}=this.state
-    const {visible,form,onCancel, onCreate} =this.props
+  render() {
+    const { channels } = this.state;
+    const { visible, form, onCancel } = this.props;
     const { getFieldDecorator } = form;
     const menu = (
-    <Menu onClick={this.handleParentChange}>
-      <Menu.Item key="0">测试</Menu.Item>
-    </Menu>
+      <Menu onClick={this.handleParentChange}>
+        <Menu.Item key="0">测试</Menu.Item>
+      </Menu>
     );
-    return(
-    <Modal
-      visible={visible}
-      title="增加角色"
-      okText="确定"
-      onCancel={onCancel}
-      onOk={this.handleSubmit}
-      destroyOnClose
-    >
-      <Form layout="vertical">
-        <FormItem label="角色名称">
-          {getFieldDecorator('name', {
-            rules: [{ required: true, message: '请输入角色名称' }],
-          })(
-          <Input />
-          )}
-        </FormItem>
-        
-        <FormItem label="上级角色">
-          {getFieldDecorator('parent', {
-          
-          })(
-          <Dropdown overlay={menu} trigger={['click']}>
-            <Button style={{ marginLeft: 8 }}>
-              请选择 <Icon type="down" />
-            </Button>
-          </Dropdown>
-          )}
-        </FormItem>
-        
-        <FormItem label="所在地区">
-          {getFieldDecorator('default_region', {
-          
-          })(<ProvinceSelect handleChange={this.handleChange}/>)}
-        </FormItem>
-        
-        <FormItem label="管理频道">
-          {getFieldDecorator('channels', {
-            rules: [{ required: true, message: '请选择频道' }],
-          })(
-          <CheckboxGroup options={channels} onChange={this.handleChanelChange} />
-          )}
-        </FormItem>
-        
-        <FormItem label="选择资源">
-          {getFieldDecorator('privileges', {
-            rules: [{ required: true, message: '请选择资源' }],
-          })(
-          <Input />
-          )}
-        </FormItem>
-      </Form>
-    </Modal>
-    )
+    return (
+      <Modal
+        visible={visible}
+        title="增加角色"
+        okText="确定"
+        onCancel={onCancel}
+        onOk={this.handleSubmit}
+        destroyOnClose
+      >
+        <Form layout="vertical">
+          <FormItem label="角色名称">
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: '请输入角色名称' }],
+            })(<Input />)}
+          </FormItem>
+
+          <FormItem label="上级角色">
+            {getFieldDecorator('parent', {})(
+              <Dropdown overlay={menu} trigger={['click']}>
+                <Button>
+                  请选择 <Icon type="down" />
+                </Button>
+              </Dropdown>
+            )}
+          </FormItem>
+
+          <FormItem label="所在地区">
+            {getFieldDecorator('default_region', {})(
+              <ProvinceSelect handleChange={this.handleChange} />
+            )}
+          </FormItem>
+
+          <FormItem label="管理频道">
+            {getFieldDecorator('channels', {
+              rules: [{ required: true, message: '请选择频道' }],
+            })(
+              <CheckboxGroup onChange={this.handleChanelChange}>
+                <Row>
+                  {channels.map(v => {
+                    return (
+                      <Col span={8} key={v.value} style={{ marginBottom: 8 }}>
+                        <Checkbox value={v.value}>{v.label}</Checkbox>
+                      </Col>
+                    );
+                  })}
+                </Row>
+              </CheckboxGroup>
+            )}
+          </FormItem>
+
+          <FormItem label="选择资源">
+            {getFieldDecorator('privileges', {
+              initialValue: this.state.menu,
+              rules: [{ required: true, message: '请选择资源' }],
+            })(<Input />)}
+          </FormItem>
+        </Form>
+      </Modal>
+    );
   }
 }
 
-export default Form.create()(AddRoleForm)
+export default Form.create()(AddRoleForm);
